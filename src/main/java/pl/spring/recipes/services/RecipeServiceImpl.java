11cd3751881d4b4b452c642bdce66eb67b0c4de1
Anna Wojcik <aaswieboda@gmail.com>
2019-09-16 14:10:42 +0200

    adding lombok, refactoring with lombok

--- src/main/java/pl/spring/recipes/services/RecipeServiceImpl.java
+++ src/main/java/pl/spring/recipes/services/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package pl.spring.recipes.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.spring.recipes.domain.Recipe;
 import pl.spring.recipes.repositories.RecipeRepository;
@@ -10,6 +11,7 @@ import java.util.Set;
 /**
  * Created by A.Wójcik on 03.09.2019.
  */
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -21,6 +23,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
+
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
